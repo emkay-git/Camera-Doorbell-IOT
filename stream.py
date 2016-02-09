0d2fea100bec23d013c0f0555987ccd51275794d
emkay-git <emkayatiitr@gmail.com>
2016-02-10 01:18:29 +0530

    VideoStream using flask and opencv

---
+++ stream.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+from flask import Flask, render_template, Response
+import cv2
+import sys
+import numpy
+# emulated camera
+
+
+# Raspberry Pi camera module (requires picamera package)
+# from camera_pi import Camera
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+def gen():
+    i=1
+    while i<10:
+        yield (b'--frame\r\n'
+            b'Content-Type: text/plain\r\n\r\n'+str(i)+b'\r\n')
+        i+=1
+
+def get_frame():
+
+    camera_port=0
+
+    ramp_frames=100
+
+    camera=cv2.VideoCapture(camera_port) #this makes a web cam object
+
+    
+    i=1
+    while True:
+        retval, im = camera.read()
+        imgencode=cv2.imencode('.jpg',im)[1]#,encode_param)
+        stringData=imgencode.tostring()
+        yield (b'--frame\r\n'
+            b'Content-Type: text/plain\r\n\r\n'+stringData+b'\r\n')
+        i+=1
+
+    del(camera)
+
+@app.route('/calc')
+def calc():
+     return Response(get_frame(),mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+if __name__ == '__main__':
+    app.run(host='172.25.32.138', debug=True, threaded=True)
